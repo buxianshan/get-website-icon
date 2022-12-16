67c832cd65bf3c98df29468a290c2400fcb47ee7
buxianshan <583425306@qq.com>
2022-12-16 14:48:42 +0800

    refactor: 拆分静态资源

---
+++ static/js/index.js
@@ -0,0 +1,43 @@
+let loading = `
+    <div id="loading" className="d-flex justify-content-center">
+        <div className="spinner-border" role="status">
+            <span className="visually-hidden">Loading...</span>
+        </div>
+    </div>`
+
+$(function () {
+    $("#searchIcon").click(function () {
+        // 清空图标
+        $("#container").empty()
+        // 添加loading元素
+        $("#container").append($(loading))
+        let target = $("input").val()
+        $.ajax({
+            url: "/get?url=" + target,
+            type: "GET",
+            dataType: "json",
+            data: {},
+            success: function (data) {
+                $("#container").empty()
+                for (let i of data.data) {
+                    let split = i.split("/")
+                    let fileName = split[split.length - 1]
+                    let div = $(`
+                            <a class="tile" title="${fileName}" target="_blank" href="${i}">
+                                <div class="tile-icon">
+                                    <img src="${i}">
+                                </div>
+                                <div class="tile-title title-ltr">
+                                    <span>${fileName}</span>
+                                </div>
+                            </a>`)
+                    $("#container").append(div)
+                }
+            },
+            error: function (e) {
+                $("#container").empty()
+                console.log(e)
+            }
+        })
+    });
+});
