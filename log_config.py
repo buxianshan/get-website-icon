f8dc2df1fb4a00fe3ec1b927df90c983a0f7ab6e
buxianshan <583425306@qq.com>
2023-01-08 16:44:20 +0800

    fix: 日志文件绝对路径

--- log_config.py
+++ log_config.py
@@ -1,6 +1,7 @@
 import logging
 from loguru import logger
 import platform
+import os
 
 
 # create a custom handler
@@ -10,7 +11,8 @@ class InterceptHandler(logging.Handler):
         logger_opt.log("DEBUG", record.getMessage())
 
 
-logger.add("icon-server.log")
+ROOT_PATH = os.path.dirname(os.path.realpath(__file__))
+logger.add(f"{ROOT_PATH}/icon-server.log")
 logging.basicConfig(handlers=[InterceptHandler()], level='INFO')
 
 # 如果是linux系统，则日志不记录基本的请求响应信息
